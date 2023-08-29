d9c5124f0ad3be6cdd81fb56ff8598f96292c5e0
Joe Shymanski <joe-shymanski@utulsa.edu>
2023-08-29 14:25:46 -0500

    Added checks for piece to move, capture, and king checks

--- chess_puzzle_explainer.py
+++ chess_puzzle_explainer.py
@@ -53,14 +53,35 @@ class Chess_Chat_GUI:
             square = chess.square(7 - file, 7 - rank) if self.human_color == chess.BLACK else chess.square(file, rank)
         return square
 
-    def create_board_image(self, board: chess.Board, move, from_square):
+    def get_pieces_captures_and_checks(self, uci_moves):
+        pieces = []
+        captures = []
+        checks = []
+        board = self.board.copy()
+        for uci_move in uci_moves:
+            uci_piece = uci_move[:2]
+            pieces.append(board.piece_at(chess.parse_square(uci_piece)))
+
+            uci_capture = uci_move[2:]
+            captures.append(board.piece_at(chess.parse_square(uci_capture)))
+
+            board.push(chess.Move.from_uci(uci_move))
+            checks.append(board.is_check())
+        return pieces, captures, checks
+
+    def create_board_image(self):
+        # Get last move, if it exists
+        try:
+            lastmove = self.board.peek()
+        except:
+            lastmove = None
         # Convert board.svg to board.png
         drawing = chess.svg.board(
-            board,
+            self.board,
             orientation = chess.WHITE if self.human_color is None else self.human_color,
-            lastmove = move,
-            check = board.king(board.turn) if board.is_check() else None,
-            fill = {from_square: "#cc0000cc"},
+            lastmove = lastmove,
+            check = self.board.king(self.board.turn) if self.board.is_check() else None,
+            fill = {self.from_square: "#cc0000cc"},
             size = self.board_size
         )
         f = open("board.svg", 'w')
@@ -112,53 +133,52 @@ class Chess_Chat_GUI:
 
     def play_game(self, human_color = None):
         # Initialize game variables
-        board = chess.Board()
+        self.board = chess.Board()
         self.human_color = human_color
         if self.human_color is None:
-            self.chats.append("CPU vs. CPU game")
+            self.chats.append("CPU vs. CPU game.")
         else:
-            self.chats.append("You are playing as " + ("White" if self.human_color == chess.WHITE else "Black"))
-        from_square = None
-        move = chess.Move.null()
+            self.chats.append("You are playing as " + ("White." if self.human_color == chess.WHITE else "Black."))
+        self.from_square = None
 
         # Game loop
         running = True
         while running:
             # Make computer move
-            if board.turn != self.human_color:
-                move = self.engine.play(board, chess.engine.Limit(time = .25)).move
-                board.push(move)
+            if self.board.turn != self.human_color:
+                move = self.engine.play(self.board, chess.engine.Limit(time = .25)).move
+                self.board.push(move)
 
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     running = False
                 
-                if event.type == pg.MOUSEBUTTONDOWN and board.turn == self.human_color:
+                if event.type == pg.MOUSEBUTTONDOWN and self.board.turn == self.human_color:
                     # Get the mouse position and corresponding square
                     x, y = pg.mouse.get_pos()
                     square = self.get_square(x, y)
                     
                     # Determine move
                     if square is None:
-                        from_square = None
-                    elif from_square is None:
-                        from_square = square if board.color_at(square) == self.human_color else None
-                    elif from_square == square:
-                        from_square = None
+                        self.from_square = None
+                    elif self.from_square is None:
+                        self.from_square = square if self.board.color_at(square) == self.human_color else None
+                    elif self.from_square == square:
+                        self.from_square = None
                     else:
                         # Make the move if it's legal
                         try:
-                            move = board.find_move(from_square, square)
+                            move = self.board.find_move(self.from_square, square)
                         except:
-                            from_square = square if board.color_at(square) == self.human_color else None
+                            self.from_square = square if self.board.color_at(square) == self.human_color else None
                         else:
-                            board.push(move)
-                            from_square = None
+                            self.board.push(move)
+                            self.from_square = None
             
             # End game
-            if board.is_game_over():
+            if self.board.is_game_over():
                 running = False
-                result = board.result()
+                result = self.board.result()
                 if result == "1-0":
                     self.chats.append("White wins!")
                 elif result == "0-1":
@@ -167,22 +187,22 @@ class Chess_Chat_GUI:
                     self.chats.append("It's a draw!")
 
             # Draw the frame
-            board_image = self.create_board_image(board, move, from_square)
+            board_image = self.create_board_image()
             self.draw_frame(board_image)
             
             # Set framerate
             self.clock.tick(60)
 
-    def play_puzzle(self, board: chess.Board, uci_moves, theme):
+    def play_puzzle(self, uci_moves, theme):
         # Initialize puzzle variables
-        self.human_color = board.turn
-        self.chats.append("You are playing as " + ("White" if self.human_color == chess.WHITE else "Black"))
-        from_square = None
-        move = chess.Move.null()
+        self.human_color = self.board.turn
+        self.chats.append("You are playing as " + ("White." if self.human_color == chess.WHITE else "Black."))
+        self.from_square = None
+        pieces, captures, checks = self.get_pieces_captures_and_checks(uci_moves)
         move_num = 0
         start_time = proactive_start = time()
-        proactive_timeout = 10
-        display_theme = False
+        proactive_timeout = 2
+        display_theme = display_piece = display_capture = display_check = False
         num_mistakes = 0
         q = False
 
@@ -190,52 +210,72 @@ class Chess_Chat_GUI:
         running = True
         while running:
             # Make computer move
-            if board.turn != self.human_color:
+            if self.board.turn != self.human_color:
                 move = chess.Move.from_uci(uci_moves[move_num])
-                board.push(move)
+                self.board.push(move)
                 move_num += 1
                 proactive_start = time()
+                display_piece = display_capture = display_check = False
 
             # Check if human is stuck
             if time() - proactive_start > proactive_timeout and not display_theme:
-                self.chats.append("Look for a " + theme)
+                self.chats.append("Look for a " + theme + ".")
                 display_theme = True
+                proactive_start = time()
+            if time() - proactive_start > proactive_timeout and not display_check:
+                check = checks[move_num]
+                if check:
+                    self.chats.append("Look to check their king.")
+                    display_check = True
+                    proactive_start = time()
+            if time() - proactive_start > proactive_timeout and not display_piece:
+                piece = pieces[move_num]
+                if piece is not None:
+                    self.chats.append("Look to move your " + chess.piece_name(piece.piece_type) + ".")
+                    display_piece = True
+                    proactive_start = time()
+            if time() - proactive_start > proactive_timeout and not display_capture:
+                capture = captures[move_num]
+                if capture is not None:
+                    self.chats.append("Look to capture their " + chess.piece_name(capture.piece_type) + ".")
+                    display_capture = True
+                    proactive_start = time()
 
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     running = False
                     q = True
 
-                if event.type == pg.MOUSEBUTTONDOWN and board.turn == self.human_color:
+                if event.type == pg.MOUSEBUTTONDOWN and self.board.turn == self.human_color:
                     # Get the mouse position and corresponding square
                     x, y = pg.mouse.get_pos()
                     square = self.get_square(x, y)
                     
                     # Determine move
                     if square is None:
-                        from_square = None
-                    elif from_square is None:
-                        from_square = square if board.color_at(square) == self.human_color else None
-                    elif from_square == square:
-                        from_square = None
+                        self.from_square = None
+                    elif self.from_square is None:
+                        self.from_square = square if self.board.color_at(square) == self.human_color else None
+                    elif self.from_square == square:
+                        self.from_square = None
                     else:
                         # Make the move if it's legal
                         try:
-                            human_move = board.find_move(from_square, square)
+                            human_move = self.board.find_move(self.from_square, square)
                         except:
-                            from_square = square if board.color_at(square) == self.human_color else None
+                            self.from_square = square if self.board.color_at(square) == self.human_color else None
                         else:
                             if human_move.uci() == uci_moves[move_num]:
                                 move = human_move
-                                board.push(move)
+                                self.board.push(move)
                                 move_num += 1
                             else:
                                 self.chats.append("Wrong move. Try again.")
                                 num_mistakes += 1
                                 if not display_theme:
-                                    self.chats.append("Look for a " + theme)
+                                    self.chats.append("Look for a " + theme + ".")
                                     display_theme = True
-                            from_square = None
+                            self.from_square = None
 
             # End puzzle
             if move_num == len(uci_moves):
@@ -243,7 +283,7 @@ class Chess_Chat_GUI:
                 running = False
 
             # Draw the frame
-            board_image = self.create_board_image(board, move, from_square)
+            board_image = self.create_board_image()
             self.draw_frame(board_image)
 
             # Set framerate
@@ -257,22 +297,22 @@ class Chess_Chat_GUI:
         n = len(puzzles)
 
         for i, puzzle in puzzles.iterrows():
-            self.chats.append("Puzzle " + str(i + 1) + " of " + str(n))
-            board = chess.Board(puzzle["Board"])
+            self.chats.append("Puzzle " + str(i + 1) + " of " + str(n) + ".")
+            self.board = chess.Board(puzzle["Board"])
             uci_moves = puzzle["Moves"]
             theme = puzzle["Theme"]
             r = puzzle["Rating"]
-            t, m, x, q = self.play_puzzle(board, uci_moves, theme)
+            t, m, x, q = self.play_puzzle(uci_moves, theme)
             t_tot += t; m_tot += m; x_tot += x; r_tot += r
             if q:
                 break
-        print("Avg:", t_tot / n, "seconds,", m_tot / n, "mistakes,", x_tot / n, "explanations,", r_tot / n, "rating")
+        print("Avg:", t_tot / (i+1), "seconds,", m_tot / (i+1), "mistakes,", x_tot / (i+1), "explanations,", r_tot / (i+1), "rating")
 
 if __name__ == "__main__":
     # Obtain puzzles
     puzzles = pd.read_excel("puzzles.xlsx")
     puzzles["Moves"] = puzzles["Moves"].apply(lambda x: x.split()) # Convert string of moves into list
-    puzzles = puzzles.sample(10).reset_index(drop=True)
+    # puzzles = puzzles.sample(10).reset_index(drop=True)
     
     # Run puzzles in GUI
     gui = Chess_Chat_GUI()
