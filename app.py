a1c762afa1078f7e17d09d7af43dceb6a3d7d4d7
Joe Shymanski <joe-shymanski@utulsa.edu>
2023-09-18 03:47:42 -0500

    CLEAN DATABASE COMMIT: Only includes puzzles and explanations

--- app.py
+++ app.py
@@ -234,7 +234,6 @@ def consent_submit():
             
             # Assign a random intervention condition
             session["protocol"] = choice(PROTOCOLS)
-            session["protocol"] = "placebic"
             # Add to user model
             user = User.query.filter_by(mturk_id=session["mturk_id"]).first()
             user.protocol = session["protocol"]
